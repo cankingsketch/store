32ea08ab96dce3a8811021ec99cbd1bf110e1d97
cankingsketch <a12710xxx@gmail.com>
2026-07-15 03:25:24 +0800

    Auto-sync events from planner

--- events-live.js
+++ events-live.js
@@ -1,3 +1,32 @@
 /* 此檔由「展覽規劃器」每日自動產生，請勿手動編輯（會被覆蓋）。
    手動維護的活動請編輯 events-data.js。 */
-window.CANKING_EVENTS_LIVE = [];
+window.CANKING_EVENTS_LIVE = [
+  {
+    "plannerId": "aae390a9-9c74-440b-bcd7-b2f525d4f2a6",
+    "date": "2026/12/13",
+    "end": "2026-12-13",
+    "name": "台中插畫博覽會",
+    "booth": ""
+  },
+  {
+    "plannerId": "6f481dae-7bdc-4d12-a312-1a88792642b9",
+    "date": "2026/10/19",
+    "end": "2026-10-19",
+    "name": "台創祭",
+    "booth": ""
+  },
+  {
+    "plannerId": "eb1401ae-38d8-4a9f-b746-69996ef3aa32",
+    "date": "2026/8/5",
+    "end": "2026-08-05",
+    "name": "文博會",
+    "booth": ""
+  },
+  {
+    "plannerId": "c1f0d498-c2a7-42ed-985a-d3af96d8c321",
+    "date": "2026/7/8~7/11",
+    "end": "2026-07-11",
+    "name": "台北世貿文具展",
+    "booth": ""
+  }
+];
